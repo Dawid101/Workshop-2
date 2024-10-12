d19f30fcecef57321182313d40ef23419c2869d9
dawid <dawcik35@gmail.com>
2024-10-12 16:57:51 +0200

    dodawanie użytkownika

--- Workshop2/src/main/java/pl/coderslab/TaskManager.java
+++ Workshop2/src/main/java/pl/coderslab/TaskManager.java
@@ -1,7 +1,20 @@
 package pl.coderslab;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class TaskManager {
     public static void main(String[] args) {
-        
+
+        User user1=new User();
+        user1.setUserName("dawid1234");
+        user1.setPassword("user1234");
+        user1.setEmail("dawid010@gmail.com");
+
+        UserDao userDao=new UserDao();
+        userDao.create(user1);
     }
 }
