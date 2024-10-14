c45f5ce191ac85de242dfac4f58cb7acd2fbff98
dawid <dawcik35@gmail.com>
2024-10-14 20:06:25 +0200

    read and delete

--- Workshop2/src/main/java/pl/coderslab/TaskManager.java
+++ Workshop2/src/main/java/pl/coderslab/TaskManager.java
@@ -9,12 +9,24 @@ import java.sql.SQLException;
 public class TaskManager {
     public static void main(String[] args) {
 
-        User user1=new User();
-        user1.setUserName("dawid1234");
-        user1.setPassword("user1234");
-        user1.setEmail("dawid010@gmail.com");
+//        User user1=new User();
+//        user1.setUserName("dawid1234");
+//        user1.setPassword("user1234");
+//        user1.setEmail("dawid010@gmail.com");
+//        User user2 = new User();
+//        user2.setUserName("dawid010");
+//        user2.setPassword("password1");
+//        user2.setEmail("dawid010@gmail.com");
+//        User user3=new User();
+//        user3.setUserName("arek_IT");
+//        user3.setPassword("qwe123");
+//        user3.setEmail("arooo@gmail.com");
+
 
         UserDao userDao=new UserDao();
-        userDao.create(user1);
+//        userDao.create(user3);
+//        userDao.read(4);
+//        userDao.delete(3);
+        userDao.allUsers();
     }
 }
--- Workshop2/src/main/java/pl/coderslab/entity/UserDao.java
+++ Workshop2/src/main/java/pl/coderslab/entity/UserDao.java
@@ -9,10 +9,28 @@ import java.sql.*;
 public class UserDao {
     private static final String CREATE_USER_QUERY =
             "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    ;
 
     public String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
+    public String allUsers() {
+        String sqlAllUsers="SELECT * FROM users";
+       try(Connection conn = DbUtil.getConnection()){
+           PreparedStatement statement=conn.prepareStatement(sqlAllUsers);
+           ResultSet resultSet = statement.executeQuery(sqlAllUsers);
+           while (resultSet.next()) {
+               // Zakładając, że mamy dwie kolumny: id (typu int) i name (typu String)
+               int id = resultSet.getInt("id");
+               String name = resultSet.getString("username");
+               System.out.println("ID: " + id + ", Name: " + name);
+           }
+       }catch (SQLException e){
+           e.printStackTrace();
+       }
+       return null;
+    }
+
     public User create(User user) {
         try (Connection conn = DbUtil.getConnection()) {
             PreparedStatement statement =
@@ -23,13 +41,48 @@ public class UserDao {
             statement.executeUpdate();
             //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
             ResultSet resultSet = statement.getGeneratedKeys();
-            if (resultSet.next()) {
-                user.setId(resultSet.getInt(1));
-            }
+//            if (resultSet.next()) {
+//                user.setId(resultSet.getInt(1));
+//            }
+            System.out.println("Użytkownik dodany do bazy");
             return user;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public User read(int userId) {
+        String sqlRead = "SELECT * FROM users WHERE id = "+userId;
+        try(Connection conn=DbUtil.getConnection()){
+            PreparedStatement statement = conn.prepareStatement(sqlRead);
+            ResultSet resultSet = statement.executeQuery(sqlRead);
+            if (resultSet.next()) {
+                System.out.println(resultSet.getString("username"));
+            }
+            else {
+                System.out.println("Brak w bazie użytkowinka o takim ID");
+                return null;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    return null;
+    }
+    public void delete(int userId) {
+        String sqlDelete = "DELETE FROM users WHERE id = "+userId;
+        try(Connection conn=DbUtil.getConnection()){
+            PreparedStatement statement = conn.prepareStatement(sqlDelete);
+            int result = statement.executeUpdate(sqlDelete);
+            if (result == 1) {
+                System.out.println("Użytkownik o podanym ID został usunięty");
+            }
+            else {
+                System.out.println("Brak w bazie użytkowinka o takim ID");
+
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
         }
     }
 
