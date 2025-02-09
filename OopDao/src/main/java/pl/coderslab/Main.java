e94d51fd979206f2554ff2c11f87f62fceee471d
dawid101 <dawcik35@gmail.com>
2025-02-09 12:55:39 +0100

    Utworzenie bazy oraz tabeli, połączenie przez DbUtil

--- OopDao/src/main/java/pl/coderslab/Main.java
+++ OopDao/src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,14 @@
 package pl.coderslab;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try(Connection conn = DbUtil.getConnection()){
+            System.out.println("Connected to database");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
