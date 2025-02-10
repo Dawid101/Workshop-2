54bf6a18561cc517617a70f2a191509415067c1d
dawid101 <dawcik35@gmail.com>
2025-02-10 21:16:15 +0100

    Utworzenie metody create oraz read

--- OopDao/src/main/java/pl/coderslab/Main.java
+++ OopDao/src/main/java/pl/coderslab/Main.java
@@ -1,14 +1,19 @@
 package pl.coderslab;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        try(Connection conn = DbUtil.getConnection()){
-            System.out.println("Connected to database");
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+        User user1 = new User();
+        user1.setUsername("Olcia19");
+        user1.setPassword("1234");
+        user1.setEmail("olcia19@gmail.com");
+
+        UserDao userDao = new UserDao();
+        System.out.println(userDao.read(3).toString());
     }
 }
---
+++ OopDao/src/main/java/pl/coderslab/entity/UserDao.java
@@ -0,0 +1,56 @@
+package pl.coderslab.entity;
+
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.DbUtil;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class UserDao {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    private static final String READ_USER_QUERY =
+            "SELECT * FROM users WHERE id = ?";
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public User read(int userId) {
+        User user = new User();
+        try(Connection conn  = DbUtil.getConnection()){
+            PreparedStatement preparedStatement = conn.prepareStatement(READ_USER_QUERY);
+            preparedStatement.setInt(1, userId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+                user.setUsername(resultSet.getString(2));
+                user.setEmail(resultSet.getString(3));
+            }
+            return user;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
